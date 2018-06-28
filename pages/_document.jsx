56780c10e67503fee29dbdeba059426c8cb24bd9
Manthan Mallikarjun <mmallikarjun@ebates.com>
2018-06-28 15:15:53 -0700

    add favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -11,6 +11,7 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="description" content="Ebates is built on top of many open source technologies. Here are some of the tools and libraries we have developed to give back to the community." />
           <meta name="author" content="Ebates" />
+          <link rel="icon" href="/static/images/favicon.png" sizes="32x32" />
           <title>Open Source @ Ebates</title>
           <link rel="stylesheet" href="/_next/static/style.css" />
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossOrigin="anonymous" />
