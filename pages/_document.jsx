4b4348d0140ffab6163f24b7dee738186ececaeb
Nick Tan <68437949+rr-nick-tan@users.noreply.github.com>
2021-09-30 20:36:35 -0700

    feat: update title (#1)

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -15,7 +15,7 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" href="https://static.ebates.com/static/images/favicons-r/favicon-57x57.png" sizes="57x57" />
           <link rel="icon" type="image/png" href="https://static.ebates.com/static/images/favicons-r/favicon-72x72.png" sizes="72x72" />
           <link rel="icon" type="image/png" href="https://static.ebates.com/static/images/favicons-r/favicon-76x76.png" sizes="76x76" />
-          <title>Open Source @ Rakuten</title>
+          <title>Open Source @ Rakuten Rewards</title>
           <link rel="stylesheet" href="/_next/static/style.css" />
           <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossOrigin="anonymous" />
         </Head>
--- pages/index.jsx
+++ pages/index.jsx
@@ -138,7 +138,7 @@ export default class extends React.Component {
           <div className="hero-body has-text-centered">
             <div className="container">
               <h1 className="title">
-                Open Source @ Rakuten
+                Open Source @ Rakuten Rewards
               </h1>
               <h2 className="subtitle is-size-4">
                 Ebates is built on top of many open source technologies.
